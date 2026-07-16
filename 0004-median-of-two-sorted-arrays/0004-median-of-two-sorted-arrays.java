8dc676fe36ab7b6008f8e5a4f2ae62db67201b2d
Yash Singh <yashpratap1837@gmail.com>
2026-07-16 14:21:45 +0530

    Time: 2 ms (47.95%), Space: 49.1 MB (5.03%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -3,14 +3,12 @@ class Solution {
         int m = nums1.length;
         int n = nums2.length;
         
-        // 1. Create a new array to hold all combined elements
         int[] merged = new int[m + n];
         
-        int i = 0; // Pointer for nums1
-        int j = 0; // Pointer for nums2
-        int k = 0; // Pointer for merged array
+        int i = 0;
+        int j = 0;
+        int k = 0;
         
-        // 2. Merge elements in sorted order
         while (i < m && j < n) {
             if (nums1[i] <= nums2[j]) {
                 merged[k] = nums1[i];
@@ -22,28 +20,23 @@ class Solution {
             k++;
         }
         
-        // If there are remaining elements in nums1, copy them
         while (i < m) {
             merged[k] = nums1[i];
             i++;
             k++;
         }
         
-        // If there are remaining elements in nums2, copy them
         while (j < n) {
             merged[k] = nums2[j];
             j++;
             k++;
         }
         
-        // 3. Find the median using your exact math logic (fixed for 0-indexing)
         int totalLength = merged.length;
         
         if (totalLength % 2 != 0) {
-            // Odd length: return the exact middle element
             return (double) merged[totalLength / 2];
         } else {
-            // Even length: return average of the two middle elements
             int mid1 = merged[(totalLength / 2) - 1];
             int mid2 = merged[totalLength / 2];
             return (mid1 + mid2) / 2.0;
