791fa076b190a85dcecc20e7c1337aa03c134e06
Yash Singh <yashpratap1837@gmail.com>
2026-08-02 19:39:48 +0530

    Time: 1 ms (98.76%), Space: 47.3 MB (96.63%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -1,27 +1,21 @@
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        int len = flowerbed.length;
-        int count = 0;
-        // Handle first position
-        if (flowerbed[0] == 0 && (len == 1 || flowerbed[1] == 0)) {
-            flowerbed[0] = 1;
-            count++;
-        }
-        // Check middle positions
-        for (int i = 1; i < len - 1; i++) {
-            if (flowerbed[i] == 0 && 
-                flowerbed[i - 1] == 0 && 
-                flowerbed[i + 1] == 0) {
-
-                count++;
-                flowerbed[i] = 1;
+        int len= flowerbed.length;
+        int count=0;
+        for(int i=0;i<len;i++){
+            // Check if current position is empty
+            if(flowerbed[i]==0)
+            // Check left side and right side
+                if((i==0||flowerbed[i-1] == 0) && (i==len-1 || flowerbed[i+1] == 0)){
+                    count++;
+                    // Place flower
+                    flowerbed[i]=1;
+                }
             }
+        // If enough flowers are placed
+        if(count>=n){
+            return true;
         }
-        // Handle last position
-        if (len > 1 && flowerbed[len - 1] == 0 && flowerbed[len - 2] == 0) {
-            flowerbed[len - 1] = 1;
-            count++;
-        }
-        return count >= n;
+        return false;
     }
 }
