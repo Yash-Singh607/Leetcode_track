2b35be66e9c59cfc4f057e3286e9a2f81a7ddecb
Yash Singh <yashpratap1837@gmail.com>
2026-08-02 19:34:24 +0530

    Time: 1 ms (98.76%), Space: 47.5 MB (94.81%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -3,12 +3,16 @@ class Solution {
         int len= flowerbed.length;
         int count=0;
         for(int i=0;i<len;i++){
+            // Check if current position is empty
             if(flowerbed[i]==0)
+            // Check left side and right side
                 if((i==0||flowerbed[i-1] == 0) && (i==len-1 || flowerbed[i+1] == 0)){
                     count++;
+                    // Place flower
                     flowerbed[i]=1;
                 }
             }
+        // If enough flowers are placed
         if(count>=n){
             return true;
         }
