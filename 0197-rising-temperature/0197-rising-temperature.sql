4a42d05974c755adf5f74f6aae65606256a516c0
Yash Singh <yashpratap1837@gmail.com>
2026-08-03 12:21:10 +0530

    Time: 614 ms (13.42%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -3,3 +3,4 @@ SELECT today.id
 FROM Weather today JOIN Weather yesterday
 ON DATEDIFF(today.recordDate,yesterday.recordDate)=1
 WHERE today.temperature>yesterday.temperature
+# to find difference we have DATEDIFF for difference in dates
