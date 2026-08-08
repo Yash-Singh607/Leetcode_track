f4172957561836cafaa073da0b52e6a79a2182dd
Yash Singh <yashpratap1837@gmail.com>
2026-08-08 12:33:55 +0530

    Time: 653 ms (82.73%), Space: 0B (100%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,6 +1,9 @@
-# Write your MySQL query statement below
-SELECT s.user_id,COALESCE(ROUND(SUM(action='confirmed')/ COUNT(c.action),2),0) AS confirmation_rate FROM
-Signups s LEFT JOIN Confirmations c
-ON s.user_id=c.user_id
+SELECT s.user_id,
+       COALESCE(ROUND(SUM(c.action = 'confirmed') / COUNT(c.action), 2), 0) AS confirmation_rate
+FROM Signups s
+LEFT JOIN Confirmations c
+ON s.user_id = c.user_id
 GROUP BY s.user_id
-;
+ORDER BY s.user_id DESC;
+# we use COLAESCE(NULL,0) to fill the null values
+# to do in desc or asc use ORDER BY
