a37cb1bd1d3ebd745f2a5ac8873350bae4ddbb2c
Yash Singh <yashpratap1837@gmail.com>
2026-08-12 21:08:20 +0530

    Time: 2 ms (99.64%), Space: 48 MB (9.35%) - LeetHub

--- 0849-maximize-distance-to-closest-person/0849-maximize-distance-to-closest-person.java
+++ 0849-maximize-distance-to-closest-person/0849-maximize-distance-to-closest-person.java
@@ -1,42 +1,22 @@
 class Solution {
     public int maxDistToClosest(int[] seats) {
-
-        // Index of the last person we found
-        int last = -1;
-
-        // Stores the maximum distance found so far
-        int ans = 0;
-
-        // Traverse the seats from left to right
-        for (int i = 0; i < seats.length; i++) {
-
-            // If there is a person at index i
-            if (seats[i] == 1) {
-
-                // This is the first person we have found
-                if (last == -1) {
-
-                    // All empty seats before this person
-                    // have their closest person at index i
-                    ans = i;
-
-                } else {
-
-                    // There is a person at 'last' and another at 'i'
-                    // Alex can sit in the middle of these two people
-                    // so the maximum distance is (i - last) / 2
-                    ans = Math.max(ans, (i - last) / 2);
+        int last=-1;
+        int ans= 0;
+        for(int i=0;i<seats.length;i++){
+            if(seats[i]==1){
+                if(last==-1){
+                    ans=i;
                 }
-
-                // Update last to the current person's position
-                last = i;
+                else{
+                    ans=Math.max(ans,(i-last)/2);
+                }
+                last=i;
             }
         }
-
-        // Check the empty seats after the last person
-        // The farthest seat is the last seat: seats.length - 1
         ans = Math.max(ans, seats.length - 1 - last);
-
         return ans;
     }
 }
+//Beginning → i
+//Middle    → (i - last) / 2
+//End       → n - 1 - last
