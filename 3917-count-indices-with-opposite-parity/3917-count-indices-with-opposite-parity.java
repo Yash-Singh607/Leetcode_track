eaec70638f16f5921c1d86da8bb241e4ba0f9775
Yash Singh <yashpratap1837@gmail.com>
2026-08-12 23:26:17 +0530

    Time: 1 ms (100%), Space: 46.9 MB (49.04%) - LeetHub

--- 3917-count-indices-with-opposite-parity/3917-count-indices-with-opposite-parity.java
+++ 3917-count-indices-with-opposite-parity/3917-count-indices-with-opposite-parity.java
@@ -4,11 +4,18 @@ class Solution {
         int odd = 0;
         int even = 0;
         int[] ans = new int[n];
+        // Traverse from right to left so that
+        // odd and even store the counts of elements to the right
         for (int i = n - 1; i >= 0; i--) {
+            // If current number is even,
+            // we need the count of odd numbers after it
             if (nums[i] % 2 == 0) {
                 ans[i] = odd;
                 even++;
-            } else {
+            } 
+            // If current number is odd,
+            // we need the count of even numbers after it
+            else {
                 ans[i] = even;
                 odd++;
             }
