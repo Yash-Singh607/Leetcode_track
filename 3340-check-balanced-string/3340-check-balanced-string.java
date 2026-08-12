b7314b849cb2a090aaa3f700c763d410db1c52b3
Yash Singh <yashpratap1837@gmail.com>
2026-08-12 21:20:55 +0530

    Time: 1 ms (98.22%), Space: 43.4 MB (67.32%) - LeetHub

--- 3340-check-balanced-string/3340-check-balanced-string.java
+++ 3340-check-balanced-string/3340-check-balanced-string.java
@@ -1,19 +1,13 @@
 class Solution {
     public boolean isBalanced(String num) {
-        //int nums=Integer.parseInt(num);
-        int sum=0;
-        for(int i=0;i<num.length();i+=2){
-            sum+=Character.getNumericValue(num.charAt(i));
+        int sum = 0;
+        for (int i = 0; i < num.length(); i++) {
+            int digit = num.charAt(i) - '0';
+            if (i % 2 == 0)
+                sum += digit;
+            else
+                sum -= digit;
         }
-        int sum1=0;
-        for(int i=1;i<num.length();i+=2){
-            sum1+=Character.getNumericValue(num.charAt(i));
-        }
-        if(sum==sum1){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return sum == 0;
     }
 }
