c1a34cfc3e81b344e7aa846b6eea172f34a8cc7f
Yash Singh <yashpratap1837@gmail.com>
2026-08-14 19:16:09 +0530

    Time: 3 ms (43.02%), Space: 43.8 MB (47.03%) - LeetHub

--- 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.java
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.java
@@ -1,36 +1,21 @@
 class Solution {
     public int maximumLengthSubstring(String s) {
-        // Store the frequency (count) of each character
-        Map<Character, Integer> count = new HashMap<>();
-        // i = left pointer
-        // res = maximum length found
-        int i = 0, res = 0;
-        // j = right pointer
-        // Move j through the string
-        for (int j = 0; j < s.length(); j++) {
-            // Get the current character
-            char c = s.charAt(j);
-            // Increase the count of current character
-            // If character is not present, start with 0
-            count.put(c, count.getOrDefault(c, 0) + 1);
-            // If current character appears more than 2 times,
-            // move the left pointer to make the window valid
-            while (count.get(c) > 2) {
-                // Get the character at the left side
-                char left = s.charAt(i);
-                // Remove one occurrence of the left character
-                count.put(left, count.get(left) - 1);
-                // Move left pointer forward
+        HashMap<Character,Integer> count=new HashMap<>();
+        int i=0;
+        int res=0;
+        for(int j=0;j<s.length();j++){
+            char c=s.charAt(j);
+            count.put(c,count.getOrDefault(c,0)+1);
+            while(count.get(c)>2){
+                // decrease the window until count become less than 2
+                //shrink the window
+                char left=s.charAt(i);
+                count.put(left,count.get(left)-1);
                 i++;
             }
-
-            // Calculate current window length
-            // j - i + 1 because both i and j are included
-            // Keep the maximum length
-            res = Math.max(res, j - i + 1);
+            // if not greater than 2 then store the maximum window size in res
+            res=Math.max(res,j-i+1);
         }
-
-        // Return the longest valid substring length
         return res;
     }
 }
