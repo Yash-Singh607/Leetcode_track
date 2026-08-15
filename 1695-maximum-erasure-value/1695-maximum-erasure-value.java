5ce8ddae843fe225bc5f07e88efc9af06f765989
Yash Singh <yashpratap1837@gmail.com>
2026-08-15 20:18:45 +0530

    Time: 51 ms (69.46%), Space: 93.1 MB (64.1%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.java
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.java
@@ -1,19 +1,29 @@
-class Solution {
-    public int maximumUniqueSubarray(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-        int left = 0;
-        int sum = 0;
-        int max = 0;
-        for (int right = 0; right < nums.length; right++) {
-            while (set.contains(nums[right])) {
-                set.remove(nums[left]);
-                sum -= nums[left];
-                left++;
-            }
-            set.add(nums[right]);
-            sum += nums[right];
-            max = Math.max(max, sum);
-        }
-        return max;
-    }
+class Solution { 
+    public int maximumUniqueSubarray(int[] nums) { 
+        
+        // Stores elements currently present in the window
+        HashSet<Integer> set = new HashSet<>(); 
+        // Left boundary of the sliding window
+        int left = 0; 
+        // Sum of elements inside the current window
+        int sum = 0; 
+        // Stores the maximum sum found so far
+        int max = 0; 
+        // Expand the window using right pointer
+        for (int right = 0; right < nums.length; right++) { 
+            // If duplicate is found, shrink the window
+            // from the left until the duplicate is removed
+            while (set.contains(nums[right])) { 
+                set.remove(nums[left]); 
+                sum -= nums[left]; 
+                left++; 
+            } 
+            // Add the new unique element to the window
+            set.add(nums[right]); 
+            sum += nums[right]; 
+            // Update maximum sum
+            max = Math.max(max, sum); 
+        } 
+        return max; 
+    } 
 }
