03f2c43c0dbbf9bb6732c712469bc250d98859a8
Yash Singh <yashpratap1837@gmail.com>
2026-08-16 12:41:06 +0530

    Time: 7 ms (29.05%), Space: 114.7 MB (82.43%) - LeetHub

--- 2029-stone-game-ix/2029-stone-game-ix.java
+++ 2029-stone-game-ix/2029-stone-game-ix.java
@@ -1,21 +1,30 @@
 class Solution {
     public boolean stoneGameIX(int[] stones) {
-        int a = 0;
-        int b = 0;
-        int c = 0;
-        for (int x : stones) {
-            if (x % 3 == 0) {
-                a++;
-            } else if (x % 3 == 1) {
-                b++;
+        int count0 = 0;
+        int count1 = 0;
+        int count2 = 0;
+        // Count stones based on remainder
+        for (int stone : stones) {
+            if (stone % 3 == 0) {
+                count0++;
+            } else if (stone % 3 == 1) {
+                count1++;
             } else {
-                c++;
+                count2++;
             }
         }
-        if (a % 2 == 0) {
-            return b > 0 && c > 0;
+        // No stone with remainder 1 or 2
+        if (count1 == 0 && count2 == 0) {
+            return false;
         }
-
-        return Math.abs(b - c) > 2;
+        // Even number of remainder-0 stones
+        if (count0 % 2 == 0) {
+            return count1 > 0 && count2 > 0;
+        }
+        // Odd number of remainder-0 stones
+        return Math.abs(count1 - count2) > 2;
     }
 }
+// count0 = number of values divisible by 3
+// count1 = number with remainder 1
+// count2 = number with remainder 2
