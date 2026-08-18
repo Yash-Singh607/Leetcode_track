ce0130a834af2e799ec523c282f45a0055bb779d
Yash Singh <yashpratap1837@gmail.com>
2026-08-18 12:47:33 +0530

    Time: 0 ms (100%), Space: 43.2 MB (40.54%) - LeetHub

--- 2511-maximum-enemy-forts-that-can-be-captured/2511-maximum-enemy-forts-that-can-be-captured.java
+++ 2511-maximum-enemy-forts-that-can-be-captured/2511-maximum-enemy-forts-that-can-be-captured.java
@@ -1,31 +1,15 @@
 class Solution {
     public int captureForts(int[] forts) {
-        int ans = 0;
-        for (int i = 0; i < forts.length; i++) {
-            // Start from our fort
-            if (forts[i] == 1) {
-                int j = i + 1;
-                // Move through enemy forts
-                while (j < forts.length && forts[j] == 0) {
-                    j++;
-                }
-                // If we reach an empty position
-                if (j < forts.length && forts[j] == -1) {
-                    ans = Math.max(ans, j - i - 1);
-                }
+        int last=-1;
+        int ans=0;
+        for(int i=0;i<forts.length;i++){
+            if(forts[i]==0){
+                continue;
             }
-            // Start from an empty position
-            if (forts[i] == -1) {
-                int j = i + 1;
-                // Move through enemy forts
-                while (j < forts.length && forts[j] == 0) {
-                    j++;
-                }
-                // If we reach our fort
-                if (j < forts.length && forts[j] == 1) {
-                    ans = Math.max(ans, j - i - 1);
-                }
+            else if(last!=-1 && forts[last]!=forts[i]){
+                ans=Math.max(ans,i-last-1);
             }
+            last=i;
         }
         return ans;
     }
