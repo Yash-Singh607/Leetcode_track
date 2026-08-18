28c82275233603e460171fa021e1a61e43db0be8
Yash Singh <yashpratap1837@gmail.com>
2026-08-18 12:48:27 +0530

    Time: 0 ms (100%), Space: 43.1 MB (55.41%) - LeetHub

--- 2511-maximum-enemy-forts-that-can-be-captured/2511-maximum-enemy-forts-that-can-be-captured.java
+++ 2511-maximum-enemy-forts-that-can-be-captured/2511-maximum-enemy-forts-that-can-be-captured.java
@@ -1,15 +1,20 @@
 class Solution {
     public int captureForts(int[] forts) {
-        int last=-1;
-        int ans=0;
-        for(int i=0;i<forts.length;i++){
-            if(forts[i]==0){
+        int last = -1; // Store the previous non-zero position
+        int ans = 0;   // Store the maximum forts captured
+        for (int i = 0; i < forts.length; i++) {
+            // Skip enemy forts (0)
+            if (forts[i] == 0) {
                 continue;
             }
-            else if(last!=-1 && forts[last]!=forts[i]){
-                ans=Math.max(ans,i-last-1);
+            // If current and previous forts are different (1 and -1)
+            // then all elements between them are enemy forts
+            else if (last != -1 && forts[last] != forts[i]) {
+                // Count the enemy forts between the two positions
+                ans = Math.max(ans, i - last - 1);
             }
-            last=i;
+            // Store the current non-zero position
+            last = i;
         }
         return ans;
     }
