086d175e65ba138d2592b92425d4db7289aea74a
Yash Singh <yashpratap1837@gmail.com>
2026-08-20 11:29:03 +0530

    Time: 1 ms (98.08%), Space: 46.6 MB (66.35%) - LeetHub

--- 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.java
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.java
@@ -1,21 +1,41 @@
-class Solution {
-    public int[] resultArray(int[] nums) {
-        ArrayList<Integer> list1=new ArrayList<>();
-        list1.add(nums[0]);
-        ArrayList<Integer> list2=new ArrayList<>();
-        list2.add(nums[1]);
-        for(int i=2;i<nums.length;i++){
-            if(list1.get(list1.size()-1)>list2.get(list2.size()-1)){
-                list1.add(nums[i]);
-            }
-            else{
-                list2.add(nums[i]);
-            }
-        }
-        list1.addAll(list2);
-        for(int i=0;i<nums.length;i++){
-            nums[i]=list1.get(i);
-        }
-        return nums;
-    }
+class Solution { 
+    public int[] resultArray(int[] nums) { 
+        int n = nums.length; 
+        // create two arrays to store the elements
+        int[] arr1 = new int[n]; 
+        int[] arr2 = new int[n]; 
+        int a = 0; 
+        int b = 0; 
+        // put the first element in arr1 and second in arr2
+        arr1[a] = nums[0]; 
+        arr2[b] = nums[1]; 
+        a++; 
+        b++; 
+        // start from the third element and compare last elements
+        for (int i = 2; i < n; i++) { 
+            // if last element of arr1 is greater, add current element to arr1
+            if (arr1[a - 1] > arr2[b - 1]) { 
+                arr1[a] = nums[i]; 
+                a++; 
+            } else { 
+                // otherwise add current element to arr2
+                arr2[b] = nums[i]; 
+                b++; 
+            } 
+        } 
+        // combine arr1 and arr2 into the result array
+        int[] result = new int[n]; 
+        int k = 0; 
+        // first copy all elements of arr1
+        for (int i = 0; i < a; i++) { 
+            result[k] = arr1[i]; 
+            k++; 
+        } 
+        // then copy all elements of arr2
+        for (int j = 0; j < b; j++) { 
+            result[k] = arr2[j]; 
+            k++; 
+        } 
+        return result; 
+    } 
 }
