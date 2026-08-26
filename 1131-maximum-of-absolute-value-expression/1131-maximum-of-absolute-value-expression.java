741bd72a115844ceb1bf5006911598942d2cbd90
Yash Singh <yashpratap1837@gmail.com>
2026-08-26 10:49:38 +0530

    Time: 5 ms (79.53%), Space: 53.5 MB (17.21%) - LeetHub

--- 1131-maximum-of-absolute-value-expression/1131-maximum-of-absolute-value-expression.java
+++ 1131-maximum-of-absolute-value-expression/1131-maximum-of-absolute-value-expression.java
@@ -1,25 +1,30 @@
 class Solution {
     public int maxAbsValExpr(int[] arr1, int[] arr2) {
         int n = arr1.length;
-        int res = 0;
-        int[][] sign = {
-                { 1, 1 },
-                { -1, 1 },
-                { 1, -1 },
-                { -1, -1 }
+        int result = 0;
+        // |x| can be written as +x or -x, so try all 4 sign combinations
+        int[][] signs = {
+            {1, 1},
+            {-1, 1},
+            {1, -1},
+            {-1, -1}
         };
-        for (int[] s : sign) {
-            int max = Integer.MIN_VALUE;
-            int min = Integer.MAX_VALUE;
+        for (int[] sign : signs) {
+            int maxValue = Integer.MIN_VALUE;
+            int minValue = Integer.MAX_VALUE;
             for (int i = 0; i < n; i++) {
-                int val = s[0] * arr1[i] + s[1] * arr2[i] + i;
-                if (val > max)
-                    max = val;
-                if (val < min)
-                    min = val;
+                // Create one signed expression for the current index
+                int value = sign[0] * arr1[i]
+                          + sign[1] * arr2[i]
+                          + i;
+                // Store maximum and minimum to avoid looping over j
+                maxValue = Math.max(maxValue, value);
+                minValue = Math.min(minValue, value);
             }
-            res = Math.max(res, max - min);
+            // max - min gives the maximum difference between any two indices
+            // This replaces the O(n²) i,j pair checking
+            result = Math.max(result, maxValue - minValue);
         }
-        return res;
+        return result;
     }
 }
