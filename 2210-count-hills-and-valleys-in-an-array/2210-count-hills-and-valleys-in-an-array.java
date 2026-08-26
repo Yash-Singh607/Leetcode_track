8dc85a50f1d2e44e723ffd1711db0518552f5261
Yash Singh <yashpratap1837@gmail.com>
2026-08-26 23:16:19 +0530

    Time: 1 ms (50.06%), Space: 43.6 MB (16.52%) - LeetHub

--- 2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.java
+++ 2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.java
@@ -1,22 +1,28 @@
 class Solution {
     public int countHillValley(int[] nums) {
-        int n = nums.length;
         int count = 0;
-        int i = 0;
-        int j = 1;
-        while (j < n - 1) {
-            // Skip duplicate values
-            if (nums[j] == nums[j + 1]) {
-                j++;
+        for (int i = 1; i < nums.length - 1; i++) {
+            // Skip if current value is same as the previous value
+            if (nums[i - 1] == nums[i]) {
                 continue;
             }
-            // Check hill or valley
-            if ((nums[j] > nums[i] && nums[j] > nums[j + 1]) ||
-                (nums[j] < nums[i] && nums[j] < nums[j + 1])) {
-                count++;
-                i = j;
+            // Find the next different value to handle duplicates
+            int next = i + 1;
+            while (next < nums.length && nums[next] == nums[i]) {
+                next++;
             }
-            j++;
+            // No different value exists on the right
+            if (next == nums.length) {
+                break;
+            }
+            // Hill: left < current > right
+            if (nums[i - 1] < nums[i] && nums[i] > nums[next]) {
+                count++;
+            }
+            // Valley: left > current < right
+            else if (nums[i - 1] > nums[i] && nums[i] < nums[next]) {
+                count++;
+            }
         }
         return count;
     }
