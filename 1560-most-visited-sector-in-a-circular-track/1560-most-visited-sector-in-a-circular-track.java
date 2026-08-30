71363390825f5cfbdc4e7068d3e0afc7722c102c
Yash Singh <yashpratap1837@gmail.com>
2026-08-30 19:42:40 +0530

    Time: 1 ms (48.13%), Space: 44.7 MB (37.97%) - LeetHub

--- 1560-most-visited-sector-in-a-circular-track/1560-most-visited-sector-in-a-circular-track.java
+++ 1560-most-visited-sector-in-a-circular-track/1560-most-visited-sector-in-a-circular-track.java
@@ -1,18 +1,21 @@
 class Solution {
     public List<Integer> mostVisited(int n, int[] rounds) {
-        int start=rounds[0];
-        int end=rounds[rounds.length-1];
-        ArrayList<Integer> list=new ArrayList<>();
-        if(start<=end){
-            for(int i=start;i<=end;i++){
+        int start = rounds[0];
+        int end = rounds[rounds.length - 1];
+        ArrayList<Integer> list = new ArrayList<>();
+        // start <= end → start to end
+        // start > end → start to n + 1 to end
+        if (start <= end) {
+            // Direct path: start to end
+            for (int i = start; i <= end; i++) {
                 list.add(i);
             }
-        }
-        else {
-            for(int i=1;i<=end;i++){
+        } else {
+            // Circular path: 1 to end, then start to n
+            for (int i = 1; i <= end; i++) {
                 list.add(i);
             }
-            for(int i=start;i<=n;i++){
+            for (int i = start; i <= n; i++) {
                 list.add(i);
             }
         }
