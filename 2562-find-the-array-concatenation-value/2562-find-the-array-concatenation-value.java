91871198b09f9b25477fa1bde77ee54229cb1e48
Yash Singh <yashpratap1837@gmail.com>
2026-09-03 17:34:07 +0530

    Time: 5 ms (51.08%), Space: 46.1 MB (46.91%) - LeetHub

--- 2562-find-the-array-concatenation-value/2562-find-the-array-concatenation-value.java
+++ 2562-find-the-array-concatenation-value/2562-find-the-array-concatenation-value.java
@@ -1,16 +1,19 @@
 class Solution {
     public long findTheArrayConcVal(int[] nums) {
-        int left=0;
-        int right=nums.length-1;
-        long concat=0;
-        while(left<right){
-            String n=Integer.toString(nums[left])+Integer.toString(nums[right]);
-            concat+=Long.parseLong(n);
+        int left = 0;
+        int right = nums.length - 1;
+        long concat = 0;
+        while (left < right) {
+            // Convert both numbers to String and concatenate them
+            String n = Integer.toString(nums[left])+ Integer.toString(nums[right]);
+            // Convert the concatenated String back to long
+            concat += Long.parseLong(n);
             left++;
             right--;
         }
-        if(left==right){
-            concat+=nums[left];
+        // If one middle element remains, add it directly
+        if (left == right) {
+            concat += nums[left];
         }
         return concat;
     }
