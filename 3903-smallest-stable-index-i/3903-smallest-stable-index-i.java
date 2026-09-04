25b971146e714463f4903d6d4d20443c7fe636b4
Yash Singh <yashpratap1837@gmail.com>
2026-09-04 13:56:46 +0530

    Time: 1 ms (99.79%), Space: 46.1 MB (88.3%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
@@ -1,19 +1,26 @@
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
-        int n=nums.length;
-        for(int i=0;i<n;i++){
-            int min=nums[i];
-            int max=nums[i];
-            for(int j=0;j<i;j++){
-                max=Math.max(max,nums[j]);
+        int n = nums.length;
+        // Check every index as a possible stable index
+        for (int i = 0; i < n; i++) {
+            // Initially consider nums[i] as both min and max
+            int min = nums[i];
+            int max = nums[i];
+            // Find the maximum value in the left part [0 ... i-1]
+            for (int j = 0; j < i; j++) {
+                max = Math.max(max, nums[j]);
             }
-            for(int j=i;j<n;j++){
-                min=Math.min(min,nums[j]);
+            // Find the minimum value in the right part [i ... n-1]
+            for (int j = i; j < n; j++) {
+                min = Math.min(min, nums[j]);
             }
-            if(max-min <= k){
-                return i;
+            // Index i is stable if the difference between
+            // maximum on the left and minimum on the right is <= k
+            if (max - min <= k) {
+                return i;  // Return the first stable index
             }
         }
+        // No stable index was found
         return -1;
     }
 }
