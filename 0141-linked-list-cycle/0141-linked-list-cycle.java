4a717ac09b800f4081aae3cae483a2aecb4b5df4
YASH SINGH <yashpratap1837@gmail.com>
2026-09-19 11:13:58 +0530

    Time: 0 ms (100%), Space: 46.9 MB (16.47%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -14,14 +14,12 @@ public class Solution {
         ListNode fast=head;
         ListNode slow=head;
         while(fast!=null && fast.next!=null){
-            fast=fast.next.next;
             slow=slow.next;
-            if(fast == slow){
+            fast=fast.next.next;
+            if(fast==slow){
                 return true;
             }
         }
         return false;
-        
-
     }
 }
