1401089133b4352d3cb1b56305e8bb0e7d9d3fad
YASH SINGH <yashpratap1837@gmail.com>
2026-09-21 14:35:12 +0530

    Time: 12 ms (92.31%), Space: 97.7 MB (21.15%) - LeetHub

--- 3524-find-x-value-of-array-i/3524-find-x-value-of-array-i.java
+++ 3524-find-x-value-of-array-i/3524-find-x-value-of-array-i.java
@@ -1,22 +1,19 @@
 class Solution {
-    public long[] resultArray(int[] nums, int k) {
-        long[] result = new long[k];
-        long[] dp = new long[k];
-        for (int num : nums) {
-            long[] newDp = new long[k];
-            // Start a new subarray with only num
-            newDp[num % k]++;
-            // Extend previous subarrays
-            for (int r = 0; r < k; r++) {
-                int newR = (int) ((1L * r * (num % k)) % k);
-                newDp[newR] += dp[r];
+    public long[] resultArray(int[] A, int k) {
+        long[] res = new long[k];
+        int[] freq = new int[k];
+        for (int n : A) {
+            n %= k;
+            int[] cur = new int[k];
+            cur[n] = 1;
+            for (int x = 0; x < k; x++) {
+                cur[x * n % k] += freq[x];
             }
-            // Add all subarrays ending here
-            for (int r = 0; r < k; r++) {
-                result[r] += newDp[r];
+            freq = cur;
+            for (int x = 0; x < k; x++) {
+                res[x] += freq[x];
             }
-            dp = newDp;
         }
-        return result;
+        return res;
     }
 }
